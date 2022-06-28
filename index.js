b95acb0a38e87d881961057775eda9a5ebb464f2
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-28 18:37:14 +0300

    commit tree 28a5d7466528076c7b820588020eb14f12306cef

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your Circle class here
+class Circle{
+    constructor(radius){
+        this.radius = radius;
+    }
+    get area(){
+        return Math.PI * this.radius * this.radius;
+    }
+    get circumference(){
+        return 2 * Math.PI * this.radius;
+    }
+    get diameter(){
+        return 2 * this.radius;
+    }
+    set diameter(diameter){
+        this.radius = diameter / 2;
+    }
+    set area(area){
+        this.radius = Math.sqrt(area / Math.PI);
+    }
+    set circumference(circumference){
+        this.radius = circumference / (2 * Math.PI);
+    }
+}
